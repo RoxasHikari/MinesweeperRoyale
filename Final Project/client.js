8a3e263b1fd950c5fad7d593e710df1deff3b1db
RoxasHikari <josh.gunville@gmail.com>
2019-11-13 10:51:01 -0500

    slightly more UI+added base Vue

---
+++ Final Project/client.js
@@ -0,0 +1,12 @@
+var vm = new Vue({
+    el: "#app",
+    data: {
+        chatLog: "",
+        playerList: [],
+        currentPlayer: "",
+    },
+    methods: {
+    },
+    computed:{
+    }
+});
